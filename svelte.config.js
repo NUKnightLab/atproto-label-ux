b06e994b72415422288eda1e152500d6b27368ce
Joe Germuska <joe@germuska.com>
2026-05-03 10:24:46 -0500

    ok vercel is picky about node version. Claude says they take 22

--- svelte.config.js
+++ svelte.config.js
@@ -1,2 +1,2 @@
-import adapter from '@sveltejs/adapter-auto';
-export default { kit: { adapter: adapter() } };
+import adapter from '@sveltejs/adapter-vercel';
+export default { kit: { adapter: adapter({ runtime: 'nodejs22.x' }) } };
